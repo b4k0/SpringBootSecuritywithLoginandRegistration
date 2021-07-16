c994da72da480208547604f80eab0ecc641b91c0
Mpak1996 <giannismpak@outlook.com>
2021-07-16 16:50:52 +0300

    Update application.properties and Upload Role.java and User.java

---
+++ src/main/java/com/springboot/demo/model/Role.java
@@ -0,0 +1,30 @@
+package com.springboot.demo.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
