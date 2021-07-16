a5ec9866780991428a99d24063bb7b4aba8b4e75
Mpak1996 <giannismpak@outlook.com>
2021-07-16 12:18:29 +0300

    First commit with deafult packages

---
+++ src/test/java/com/springboot/demo/SpringbootSecurityLoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityLoginAndRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
