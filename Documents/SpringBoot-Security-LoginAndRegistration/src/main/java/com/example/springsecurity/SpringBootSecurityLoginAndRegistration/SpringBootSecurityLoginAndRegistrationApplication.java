c5fe68bce3cf2faa98c89c051663e1d878a6cff5
Mpak1996 <giannismpak@outlook.com>
2021-09-08 22:12:24 +0300

    First JS programm with console.log

---
+++ Documents/SpringBoot-Security-LoginAndRegistration/src/main/java/com/example/springsecurity/SpringBootSecurityLoginAndRegistration/SpringBootSecurityLoginAndRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.example.springsecurity.SpringBootSecurityLoginAndRegistration;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLoginAndRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityLoginAndRegistrationApplication.class, args);
+    }
+
+}
---
+++ Documents/SpringBoot-Security-LoginAndRegistration/src/test/java/com/example/springsecurity/SpringBootSecurityLoginAndRegistration/SpringBootSecurityLoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.SpringBootSecurityLoginAndRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityLoginAndRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
